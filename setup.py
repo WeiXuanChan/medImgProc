3fda02396e433cec68dfdb2b28d4e3dfafc8c935
W. X. Chan <w.x.chan1986@gmail.com>
2019-09-05 17:08:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'medImgProc',         # How you named your package folder (MyLib)
   packages = ['medImgProc'],   # Chose the same as "name"
-  version = '1.0.0',      # Start with a small number and increase it with every change you make
+  version = '1.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Medical Image Processing module for viewing and editing.',   # Give a short description about your library
   author = 'Wei Xuan Chan',                   # Type in your name

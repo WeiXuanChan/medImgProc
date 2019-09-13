3b8365ec30f9e61c85500a06513eb1df7cbd0cf8
W. X. Chan <w.x.chan1986@gmail.com>
2019-09-13 11:10:23 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,10 @@ History:
     Date    Programmer SAR# - Description
     ---------- ---------- ----------------------------
   Author: w.x.chan@gmail.com         12JAN2018           - Created
+  Author: w.x.chan@gmail.com         12JAN2018           - v1.3.0
+                                                            -processFunc v1.3.0
+                                                            -Image v1.0.0
+                                                            -GUI v1.0.0
 
 Requirements:
     numpy.py
@@ -16,7 +20,7 @@ Known Bug:
     HSV color format not supported
 All rights reserved.
 '''
-_version='1.2.2'
+_version='1.3.0'
 print('medImgProc version',_version)
 
 

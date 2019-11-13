397e27f47c6fd486a4e9c2023a65cb8815e4d56d
W. X. Chan <w.x.chan1986@gmail.com>
2019-11-13 16:46:31 +0800

    Update __init__.py

--- medImgProc/__init__.py
+++ medImgProc/__init__.py
@@ -88,8 +88,12 @@ History:
                                                             -processFunc v1.7.3
                                                             -Image v1.8.0
                                                             -GUI v1.5.2
-  Author: w.x.chan@gmail.com         08NOV2018           - v1.8.5
+  Author: w.x.chan@gmail.com         13NOV2018           - v1.8.5
                                                             -processFunc v1.8.5
+                                                            -Image v1.8.0
+                                                            -GUI v1.5.2
+  Author: w.x.chan@gmail.com         13NOV2018           - v1.9.0
+                                                            -processFunc v1.9.0
                                                             -Image v1.8.0
                                                             -GUI v1.5.2
 
@@ -102,7 +106,7 @@ Known Bug:
     HSV color format not supported
 All rights reserved.
 '''
-_version='1.8.5'
+_version='1.9.0'
 print('medImgProc version',_version)
 
 

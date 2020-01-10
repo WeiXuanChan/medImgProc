a64fae3d3dd61662acf46c0f0f68d5cb940b09cd
W. X. Chan <w.x.chan1986@gmail.com>
2020-01-10 18:33:45 +0800

    Update __init__.py

--- medImgProc/__init__.py
+++ medImgProc/__init__.py
@@ -108,10 +108,10 @@ History:
                                                             -processFunc v2.2.8
                                                             -Image v1.8.0
                                                             -GUI v1.5.2
-  Author: w.x.chan@gmail.com         10JAN2020           - v2.3.7
+  Author: w.x.chan@gmail.com         10JAN2020           - v2.3.8
                                                             -processFunc v2.2.8
                                                             -Image v1.8.0
-                                                            -GUI v2.3.7
+                                                            -GUI v2.3.8
 
 
 Requirements:
@@ -124,7 +124,7 @@ Known Bug:
 All rights reserved.
 '''
 import logging
-_version='2.3.7'
+_version='2.3.8'
 logger = logging.getLogger('medImgProc v'+_version)
 logger.info('medImgProc version '+_version)
 

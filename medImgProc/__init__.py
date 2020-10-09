4f6248dc7ca0fcdd8c6e2244de980ac9c74b6f79
W. X. Chan <w.x.chan1986@gmail.com>
2020-10-10 07:33:39 +0800

    Update __init__.py

--- medImgProc/__init__.py
+++ medImgProc/__init__.py
@@ -187,6 +187,11 @@ History:
                                                             -Image v2.6.25
                                                             -GUI v2.6.19
                                                             -pointSpeckleProc v2.4.0
+  Author: w.x.chan@gmail.com         10Oct2020           - v2.6.31
+                                                            -processFunc v2.6.31
+                                                            -Image v2.6.25
+                                                            -GUI v2.6.19
+                                                            -pointSpeckleProc v2.4.0
 
 
 Requirements:
@@ -199,7 +204,7 @@ Known Bug:
 All rights reserved.
 '''
 import logging
-_version='2.6.30'
+_version='2.6.31'
 logger = logging.getLogger('medImgProc v'+_version)
 logger.info('medImgProc version '+_version)
 
